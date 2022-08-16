7343084fe2a5527e785871f8fac0769cd1089481
Sucram <marcuscrabtree4@gmail.com>
2022-08-16 11:32:48 -0400

    final deliverables

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(fetchPlants, []);
+
+  function fetchPlants() {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((plantsData) => setPlants(plantsData));
+  }
+
+  const displayPlants = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants} />
+      <Search setSearch={setSearch} />
+      <PlantList plants={displayPlants} />
     </main>
   );
 }
